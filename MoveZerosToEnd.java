6013d1fc6cba9391bda207de746f0e2e4373e8d3
Prashast Saxena <prasaxen@adobe.com>
2016-08-22 17:01:06 -0700

    Adding solution to \n 1. Move Zeroes to End in an arry \n 2. Reverse vowels in a string

---
+++ MoveZerosToEnd.java
@@ -0,0 +1,18 @@
+public class MoveZerosToEnd {
+
+    public int[] moveZeroesToEnd(int[] arr){
+
+        int k = 0;
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] != 0){
+                arr[k++] = arr[i];
+            }
+        }
+
+        while (k<arr.length){
+            arr[k++] = 0;
+        }
+        return arr;
+    }
+}
---
+++ ReverseVowels.java
@@ -0,0 +1,31 @@
+
+public class ReverseVowels {
+
+    public String reverseVowels(String s){
+
+        String vowels = "aeiouAEIOU";
+
+        int low = 0;
+        int high = s.length()-1;
+        char[] chars = s.toCharArray();
+
+        while (low < high){
+            if(!vowels.contains(String.valueOf(chars[low]))){
+                low ++;
+                continue;
+            }
+            if(!vowels.contains(String.valueOf(chars[high]))){
+                high--;
+                continue;
+            }
+
+            //swap
+            char t = chars[low];
+            chars[low] = chars[high];
+            chars[high] = t;
+            low++; high--;
+
+        }
+        return String.valueOf(chars);
+    }
+}
